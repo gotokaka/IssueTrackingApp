f3a6dca0097482ceee3ab642130447f68bc7a3a4
gotokaka <kakaporider510@gmail.com>
2023-06-30 20:00:36 +0900

    トランザクション処理しなかった場合の挙動を確認するコードの記述

--- src/main/java/com/example/its/domain/issue/IssueService.java
+++ src/main/java/com/example/its/domain/issue/IssueService.java
@@ -18,5 +18,8 @@ public class IssueService {
   // TODO トランザクション
   public void create(String summary, String description) {
     issueRepository.insert(summary, description);
+
+    // 後処理が増えたとする（以下、トランザクション処理をされていないとデータの整合性取れないことを確かめるExceptionのスロー）
+    throw new IllegalStateException("NG");
   }
 }
